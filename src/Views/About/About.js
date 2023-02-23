7517788a56bc1e266dd76db8b08ef1fa96af67f0
SAKSHIshete@123 <sakshishete1310@gmail.com>
2023-02-23 22:33:11 +0530

    initial commit

---
+++ src/Views/About/About.js
@@ -0,0 +1,13 @@
+import "./About.css";
+import Footer from "./../../Components/Footer/Footer";
+
+function About() {
+  return (
+    <div className="about">
+      <h1>This is About Page</h1>
+      <Footer />
+    </div>
+  )
+}
+
+export default About
---
+++ src/Views/Home/Home.js
@@ -0,0 +1,13 @@
+import "./Home.css"
+import Footer from "./../../Components/Footer/Footer"
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>This is Home Page </h1>
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
